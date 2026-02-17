c4be67a528b593b8e67c3f88442cd598e2952a3a
JoylinMathias <joylinmathias30@gmail.com>
2026-02-17 21:29:45 +0530

    update WeatherCard

--- weather-app/src/components/WeatherCard.jsx
+++ weather-app/src/components/WeatherCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
 function WeatherCard() {
     const [city, setCity] = useState("");
@@ -6,7 +7,7 @@ function WeatherCard() {
     const [desc, setDesc] = useState("");
 
     const getWeather = async () => {
-        const apiKey = "YOUR_API_KEY";
+        const apiKey = "fccaa88344492d9b763b3d7990407095";
 
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
