5d20864f1da7cd88bb0c1940baf05beed9a26488
JoylinMathias <joylinmathias30@gmail.com>
2026-02-17 21:48:47 +0530

    WeatherCard Updation

--- weather-app/src/components/WeatherCard.jsx
+++ weather-app/src/components/WeatherCard.jsx
@@ -12,6 +12,7 @@ function WeatherCard() {
 
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
         );
 
         const data = await response.json();
